145c4246b42a40f30592f8fbe04cf970b0eb90b9
Pablo Veyrat <veyratpablo@gmail.com>
2022-04-08 16:57:50 +0200

    adding scripts

--- contracts/migrator/UniMigrator.sol
+++ contracts/migrator/UniMigrator.sol
@@ -62,21 +62,24 @@ contract UniMigrator {
     ) external onlyOwner returns (address poolCreated) {
         address liquidityGauge;
         address stakingToken;
-        uint160 sqrtPriceX96Existing;
+        uint160 sqrtPriceX96;
         address token;
         if (gaugeType == 1) {
             liquidityGauge = _USDCGAUGE;
             stakingToken = _GUNIUSDC;
             token = _USDC;
-            (sqrtPriceX96Existing, , , , , , ) = IUniswapV3Pool(_UNIUSDCPOOL).slot0();
+            (sqrtPriceX96, , , , , , ) = IUniswapV3Pool(_UNIUSDCPOOL).slot0();
         } else {
             liquidityGauge = _ETHGAUGE;
             stakingToken = _GUNIETH;
             token = _WETH;
+            (sqrtPriceX96,,,,,,) =  IUniswapV3Pool(_ETHNEWPOOL).slot0();
         }
-        // Giving allowances: we need it to add and remove liquidity
+        // Allowance needed to add liquidity
         IERC20(token).safeApprove(address(_GUNIROUTER), type(uint256).max);
+        // Allowance needed to remove liquidity of the staking token
         IERC20(stakingToken).safeApprove(address(_GUNIROUTER), type(uint256).max);
+        // Allowance needed to add agEUR liquidity
         IERC20(_AGEUR).safeIncreaseAllowance(
             address(_GUNIROUTER),
             type(uint256).max - IERC20(_AGEUR).allowance(address(this), address(_GUNIROUTER))
@@ -96,20 +99,23 @@ contract UniMigrator {
             amountTokenMin,
             address(this)
         );
+        console.log("Amounts");
+        console.log(amountAgEUR, amountToken);
         if (gaugeType == 1) {
-            // In this case, it's _USDC: we need to create the pool
-            _UNI.createAndInitializePoolIfNecessary(_AGEUR, _USDC, 100, sqrtPriceX96Existing);
+            // In this case, it's USDC: we need to create the pool and initialize it at the same price
+            // as the previous pool
+            _UNI.createAndInitializePoolIfNecessary(_AGEUR, _USDC, 100, sqrtPriceX96);
             poolCreated = _GUNIFACTORY.createManagedPool(_AGEUR, _USDC, 100, 0, -276320, -273470);
         } else {
             // In this other case it's wETH
             // Increasing observation cardinality on the new pool (it already exists)
             IUniswapV3Pool(_ETHNEWPOOL).increaseObservationCardinalityNext(144);
-            // poolCreated = _GUNIFACTORY.createManagedPool(_AGEUR, _WETH, 500, 0, -96120, -70180);
 
-            // compute the lower tick to not have nearly al liquidity invested
+            // Computing the lower tick to not have nearly all liquidity invested
             int256 ratioAmounts = (int256(amountToken) * 10**18) / int256(amountAgEUR);
-            // sqrt price upper bound np.sqrt(0.000311878658174707), where 0.000311878658174707 is the current price on the pool
-            int256 sqrtPrice = 17660086584575598;
+            
+            // sqrt price of the pool
+            int256 sqrtPrice = int(uint(sqrtPriceX96) * 1e18 >> (96));
             // sqrt price upper bound np.sqrt(1/991), where 991 is just the price derived from the tick -69000
             int256 sqrtUpperPrice = 31766046899489794;
             int128 sqrtPriceLower = int128(
@@ -117,27 +123,31 @@ contract UniMigrator {
                     ((ratioAmounts * ((10**18 * 10**18) / sqrtUpperPrice - (10**18 * 10**18) / sqrtPrice)) / 10**18)
             );
             int128 priceLower = (sqrtPriceLower * sqrtPriceLower) / 10**18;
-            int128 lowerTick = ((ln(priceLower) - ln(10**18)) / (ln(1000100000000000000) - ln(10**18)));
+            int128 lowerTick = ((_ln(priceLower) - _ln(10**18)) / (_ln(1000100000000000000) - _ln(10**18)));
             lowerTick = (lowerTick / 10) * 10;
-
-            // change the ticks of the position
+            // We're creating the pool with not the final ticks: we will be rebalancing after some point
             poolCreated = _GUNIFACTORY.createManagedPool(_AGEUR, _WETH, 500, 0, int24(lowerTick), -69000);
         }
 
         // Adding liquidity
         _GUNIROUTER.addLiquidity(poolCreated, amountAgEUR, amountToken, amountAgEURMin, amountTokenMin, liquidityGauge);
 
-        IGUniPool(poolCreated).executiveRebalance(-96120, -69000, 0, 0, false);
+        if (gaugeType != 1) {
+            IGUniPool(poolCreated).executiveRebalance(-96120, -69000, 0, 0, false);
+            console.log("Amounts");
+            console.log(IERC20(_AGEUR).balanceOf(_ETHNEWPOOL), IERC20(_WETH).balanceOf(_ETHNEWPOOL));
+            /*
+            2273 093097601899487844
+            2462    478028007334003312
+            6030241 683617787583428501
+            6027137 588784044462840877
+            */
+        }
+        
         // Transfering ownership of the new pool ot AngleMaster
         IGUniPool(poolCreated).transferOwnership(0xe02F8E39b8cFA7d3b62307E46077669010883459);
 
         uint256 newGUNIBalance = IERC20(poolCreated).balanceOf(liquidityGauge);
-        console.log("agEUR leftover finally", IERC20(_AGEUR).balanceOf(address(this)));
-        console.log("Token leftover finally", IERC20(token).balanceOf(address(this)));
-        console.log("Variation in GUNI positions");
-        console.log(amountRecovered, newGUNIBalance);
-        console.log("Scaling factor");
-        console.log((amountRecovered * 10**18) / newGUNIBalance);
         ILiquidityGauge(liquidityGauge).set_staking_token_and_scaling_factor(
             poolCreated,
             (amountRecovered * 10**18) / newGUNIBalance
@@ -153,7 +163,7 @@ contract UniMigrator {
      * @param x signed 64.64-bit fixed point number
      * @return signed 64.64-bit fixed point number
      */
-    function log_2(int128 x) internal pure returns (int128) {
+    function _log2(int128 x) internal pure returns (int128) {
         unchecked {
             require(x > 0);
 
@@ -204,36 +214,11 @@ contract UniMigrator {
      * @param x signed 64.64-bit fixed point number
      * @return signed 64.64-bit fixed point number
      */
-    function ln(int128 x) internal pure returns (int128) {
+    function _ln(int128 x) internal pure returns (int128) {
         unchecked {
             require(x > 0);
 
-            return int128(int256((uint256(int256(log_2(x))) * 0xB17217F7D1CF79ABC9E3B39803F2F6AF) >> 128));
-        }
-    }
-
-    function _swapLogic(
-        uint256 ethBalance,
-        uint256 agEURBalance,
-        address poolCreated,
-        address liquidityGauge
-    ) internal {
-        if (ethBalance > (10**18) / 2) {
-            IERC20(_WETH).safeApprove(_UNIROUTER, ethBalance / 2);
-            uint256 amountOut = IUniswapV3Router(_UNIROUTER).exactInputSingle(
-                ExactInputSingleParams(_WETH, _AGEUR, 500, address(this), block.timestamp, ethBalance / 2, 0, 0)
-            );
-            _GUNIROUTER.addLiquidity(poolCreated, agEURBalance + amountOut, ethBalance / 2, 0, 0, liquidityGauge);
-            console.log("Did an ETH swap to reinstore equilibrium");
-            console.log("Amount Out", amountOut);
-        } else if (agEURBalance > 1000 * 10**18) {
-            IERC20(_AGEUR).safeApprove(_UNIROUTER, agEURBalance / 2);
-            uint256 amountOut = IUniswapV3Router(_UNIROUTER).exactInputSingle(
-                ExactInputSingleParams(_AGEUR, _WETH, 500, address(this), block.timestamp, agEURBalance / 2, 0, 0)
-            );
-            _GUNIROUTER.addLiquidity(poolCreated, agEURBalance / 2, ethBalance + amountOut, 0, 0, liquidityGauge);
-            console.log("Did an agEUR swap to reinstore equilibrium");
-            console.log("Amount Out", amountOut);
+            return int128(int256((uint256(int256(_log2(x))) * 0xB17217F7D1CF79ABC9E3B39803F2F6AF) >> 128));
         }
     }
 
